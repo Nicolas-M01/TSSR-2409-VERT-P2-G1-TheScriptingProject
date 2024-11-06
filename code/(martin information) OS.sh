4d227789cf0debbc882da6cfcf4d1e56cc7b7724
landes martin <landes_martin@yahoo.fr>
2024-11-06 19:09:38 +0000

    Rename OS.sh to (martin information) OS.sh

---
+++ code/(martin information) OS.sh
@@ -0,0 +1,2 @@
+#) 
+ echo -e "La version de cette OS est : $(cat /etc/os-release | grep -v "NAME" | grep -v "PRETTY_NAME" | grep -v "ID" | grep -v "ID_LIKE" | grep -v "HOME_URL" | grep -v "SUPPORT_URL" | grep -v "BUG_REPORT_URL" | grep -v "PRIVACY_POLICY_URL" | grep -v "UBUNTU_CODENAME")" >> ~/Documents/"info_`whoami`_$FORMATTED_DATE.txt"
